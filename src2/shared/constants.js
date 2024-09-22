677d42f8d1dd720951510e307afef6a059d1a379
TasziteOst <145514012+TasziteOst@users.noreply.github.com>
2024-09-22 07:20:46 -0300

    Delete src2 directory

--- src2/shared/constants.js
+++
@@ -1,3 +0,0 @@
-export const DEFAULT_AVATAR_URL = "https://avatar.iran.liara.run/public/48";
-export const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID;
-export const IMGUR_API_URL = "https://api.imgur.com/3/image";
--- src2/shared/utils/account/is-logged-util.js
+++
@@ -1,3 +0,0 @@
-export default function isLogged(me) {
-	return me && Object.keys(me).length > 0;
-}
